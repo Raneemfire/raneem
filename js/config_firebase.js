3f9a3a6fcd072450eeea39af9dd2ca881d957f7e
dwairi <dwairi.razan1995@gmail.com>
2017-12-09 23:05:30 +0200

    asd
    asf

---
+++ js/config_firebase.js
@@ -0,0 +1,9 @@
+ var config = {
+    apiKey: "AIzaSyCq6DYsOtJmbsXi6Ac2sS0_eu1u_ua20X4",
+    authDomain: "changer-car-electronics.firebaseapp.com",
+    databaseURL: "https://changer-car-electronics.firebaseio.com",
+    projectId: "changer-car-electronics",
+    storageBucket: "changer-car-electronics.appspot.com",
+    messagingSenderId: "143796443641"
+  };
+  firebase.initializeApp(config);
